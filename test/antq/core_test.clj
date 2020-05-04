26ca6e223dfdbf6aa9cb60a9d341e1b7589390bd
liquidz <liquidz.uo@gmail.com>
2020-05-05 08:30:57 +0900

    Fix core test

--- test/antq/core_test.clj
+++ test/antq/core_test.clj
@@ -1,7 +1,13 @@
 (ns antq.core-test
   (:require
    [antq.core :as sut]
+   [antq.record :as r]
    [clojure.test :as t]))
 
-(t/deftest greet-test
-  (t/is (= (sut/greet "world") "hello world")))
+(t/deftest skip-artifacts?-test
+  (t/are [expected in] (= expected (sut/skip-artifacts? (r/map->Dependency {:name in})))
+    true "org.clojure/clojure"
+    false "org.clojure/foo"
+    false "foo/clojure"
+    false "foo"
+    false "foo/bar"))
