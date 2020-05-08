ce055c3ffbb47207e7470c8822cad4337f46f66c
liquidz <liquidz.uo@gmail.com>
2020-05-09 06:53:40 +0900

    Add tests for util.xml

---
+++ test/antq/util/xml_test.clj
@@ -0,0 +1,34 @@
+(ns antq.util.xml-test
+  (:require
+   [antq.util.xml :as sut]
+   [clojure.java.io :as io]
+   [clojure.data.xml :as xml]
+   [clojure.test :as t]))
+
+(def ^:private test-xml
+  (-> "util/xml/dummy.xml"
+      io/resource
+      slurp
+      xml/parse-str
+      xml-seq))
+
+(def ^:private test-entries
+  (filter (comp #{:entry} :tag) test-xml))
+
+(t/deftest get-value-test
+  (t/is (= "Foo 1.0.0" (-> test-entries first :content (sut/get-value :title))))
+  (t/is (nil? (-> test-entries first :content (sut/get-value :unknown)))))
+
+(t/deftest get-values-test
+  (t/is (= ["tag:github.com,2008:Repository/928777/1.0.0" "Foo 1.0.0"]
+           (-> test-entries first :content (sut/get-values [:id :title]))))
+  (t/is (= [nil "Foo 1.0.0"]
+           (-> test-entries first :content (sut/get-values [:unknown :title]))))
+  (t/is (= [nil nil]
+           (-> test-entries first :content (sut/get-values [:unknown :unknown])))))
+
+(t/deftest get-attribute-test
+  (t/is (= "https://github.com/foo/bar/releases/tag/1.0.0"
+           (-> test-entries first :content (sut/get-attribute :link :href))))
+  (t/is (nil? (-> test-entries first :content (sut/get-attribute :link :unknown))))
+  (t/is (nil? (-> test-entries first :content (sut/get-attribute :unknown :href)))))
