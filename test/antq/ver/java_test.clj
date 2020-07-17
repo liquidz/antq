73d0fac4d33c7034237a0c9714fa3994fd5abcc5
liquidz <liquidz.uo@gmail.com>
2020-07-18 08:38:24 +0900

    Update to use tools.deps to fetch versions for java #10

--- test/antq/ver/java_test.clj
+++ test/antq/ver/java_test.clj
@@ -1,30 +1,27 @@
 (ns antq.ver.java-test
   (:require
-   [ancient-clj.core :as ancient]
    [antq.record :as r]
    [antq.ver :as ver]
-   [antq.ver.java]
+   [antq.ver.java :as sut]
    [clojure.test :as t]))
 
 (defn- dummy-versions
   [_ opts]
   (concat
-   [{:version-string "1"}]
+   ["1"]
    (when (contains? (:repositories opts) "dummy")
-     [{:version-string "1.3"}])
-   (when (:snapshots? opts)
-     [{:version-string "1.6"}])
-   [{:version-string "2"}]))
+     ["1.3"])
+   ["1.6-SNAPSHOT" "2"]))
 
 (defn- get-sorted-versions
   [m]
   (ver/get-sorted-versions (r/map->Dependency (assoc m :type :java :name "dummy"))))
 
 (t/deftest get-sorted-versions-test
-  (with-redefs [ancient/versions! dummy-versions]
-    (t/is (= ["1" "2"]
+  (with-redefs [sut/get-versions dummy-versions]
+    (t/is (= ["2" "1"]
              (get-sorted-versions {:version "1.0.0"})))
-    (t/is (= ["1" "1.6" "2"]
+    (t/is (= ["2" "1.6-SNAPSHOT" "1"]
              (get-sorted-versions {:version "1.0.0-SNAPSHOT"})))
-    (t/is (= ["1" "1.6" "2"]
+    (t/is (= ["2" "1.6-SNAPSHOT" "1"]
              (get-sorted-versions {:version "1.0.0-snapshot"})))))
