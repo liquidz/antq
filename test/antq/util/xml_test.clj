c79c34c51ec94f65e98c6a49ad0c844f97532d51
liquidz <liquidz.uo@gmail.com>
2020-07-19 00:09:24 +0900

    Fix util.xml #10

--- test/antq/util/xml_test.clj
+++ test/antq/util/xml_test.clj
@@ -13,22 +13,22 @@
       xml-seq))
 
 (def ^:private test-entries
-  (filter (comp #{:entry} :tag) test-xml))
+  (sut/get-tags :entry test-xml))
 
 (t/deftest get-value-test
-  (t/is (= "Foo 1.0.0" (-> test-entries first :content (sut/get-value :title))))
-  (t/is (nil? (-> test-entries first :content (sut/get-value :unknown)))))
+  (t/is (= "Foo 1.0.0" (->> test-entries first :content (sut/get-value :title))))
+  (t/is (nil? (->> test-entries first :content (sut/get-value :unknown)))))
 
 (t/deftest get-values-test
   (t/is (= ["tag:github.com,2008:Repository/928777/1.0.0" "Foo 1.0.0"]
-           (-> test-entries first :content (sut/get-values [:id :title]))))
+           (->> test-entries first :content (sut/get-values [:id :title]))))
   (t/is (= [nil "Foo 1.0.0"]
-           (-> test-entries first :content (sut/get-values [:unknown :title]))))
+           (->> test-entries first :content (sut/get-values [:unknown :title]))))
   (t/is (= [nil nil]
-           (-> test-entries first :content (sut/get-values [:unknown :unknown])))))
+           (->> test-entries first :content (sut/get-values [:unknown :unknown])))))
 
 (t/deftest get-attribute-test
   (t/is (= "https://github.com/foo/bar/releases/tag/1.0.0"
-           (-> test-entries first :content (sut/get-attribute :link :href))))
-  (t/is (nil? (-> test-entries first :content (sut/get-attribute :link :unknown))))
-  (t/is (nil? (-> test-entries first :content (sut/get-attribute :unknown :href)))))
+           (->> test-entries first :content (sut/get-attribute :link :href))))
+  (t/is (nil? (->> test-entries first :content (sut/get-attribute :link :unknown))))
+  (t/is (nil? (->> test-entries first :content (sut/get-attribute :unknown :href)))))
