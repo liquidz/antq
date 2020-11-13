cc15c73a296d302d6d326e1adf3dbe42103d9ebd
liquidz <liquidz.uo@gmail.com>
2020-11-14 05:36:04 +0900

    Add diff-deps to test helper

--- test/antq/test_helper.clj
+++ test/antq/test_helper.clj
@@ -1,7 +1,19 @@
 (ns antq.test-helper
   (:require
-   [antq.record :as r]))
+   [antq.record :as r]
+   [clojure.data :as data]))
 
 (defn test-dep
   [m]
   (r/map->Dependency (merge {:type :test} m)))
+
+(defn- name-version-map
+  [deps]
+  (->> deps
+       (map (juxt :name :version))
+       (into {})))
+
+(defn diff-deps
+  [from-deps to-deps]
+  (data/diff (name-version-map from-deps)
+             (name-version-map to-deps)))
