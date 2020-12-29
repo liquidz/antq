54784647ddb845654adb6790dfd5b26597cbec31
liquidz <liquidz.uo@gmail.com>
2020-12-30 08:12:59 +0900

    Fix test-helper.diff-deps to handle all diffs

--- test/antq/test_helper.clj
+++ test/antq/test_helper.clj
@@ -1,19 +1,27 @@
 (ns antq.test-helper
   (:require
    [antq.record :as r]
-   [clojure.data :as data]))
+   [antq.test-helper :as h]
+   [antq.upgrade.clojure]
+   [lambdaisland.deep-diff2 :as ddiff])
+  (:import
+   lambdaisland.deep_diff2.diff_impl.Mismatch))
 
 (defn test-dep
   [m]
   (r/map->Dependency (merge {:type :test} m)))
 
-(defn- name-version-map
+(defn name-version-sorted-list
   [deps]
   (->> deps
-       (map (juxt :name :version))
-       (into {})))
+       (map #(select-keys % [:name :version]))
+       (sort-by #(str (:name %) (:version %)))))
 
 (defn diff-deps
-  [from-deps to-deps]
-  (data/diff (name-version-map from-deps)
-             (name-version-map to-deps)))
+  [expected-deps actual-deps]
+  (->> (ddiff/diff (name-version-sorted-list expected-deps)
+                   (name-version-sorted-list actual-deps))
+       (filter #(instance? Mismatch (:version %)))
+       ;; convert `:version` to a simple map
+       (map #(update % :version (fn [m] (merge {} m))))
+       (set)))
