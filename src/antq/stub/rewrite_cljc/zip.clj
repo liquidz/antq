7edfd9fc79fb074437fb3ad0167255efdd642d39
liquidz <liquidz.uo@gmail.com>
2021-01-30 16:37:35 +0900

    Add antq.stub.rewrite-cljc.zip

---
+++ src/antq/stub/rewrite_cljc/zip.clj
@@ -0,0 +1,15 @@
+(ns antq.stub.rewrite-cljc.zip
+  "Stub rewrite-cljs.zip to support Clojure 1.8"
+  (:refer-clojure :exclude [next replace]))
+
+(def down (constantly nil))
+(def find-value (constantly nil))
+(def left (constantly nil))
+(def next (constantly nil))
+(def of-file (constantly nil))
+(def replace (constantly nil))
+(def right (constantly nil))
+(def root-string (constantly nil))
+(def tag (constantly nil))
+(def up (constantly nil))
+(def value (constantly nil))
