700409e16d365b9276289ddb1a9735273bb1954d
liquidz <liquidz.uo@gmail.com>
2021-03-19 06:09:55 +0900

    Fix reflection warnings

--- src/antq/ver/java.clj
+++ src/antq/ver/java.clj
@@ -4,12 +4,20 @@
    [antq.ver :as ver]
    [version-clj.core :as version])
   (:import
+   (org.eclipse.aether
+    DefaultRepositorySystemSession
+    RepositorySystem)
+   (org.eclipse.aether.artifact
+    Artifact)
    (org.eclipse.aether.resolution
     VersionRangeRequest)))
 
 (defn get-versions
   [name opts]
-  (let [{:keys [system session artifact remote-repos]} (u.mvn/repository-system name "[0,)" opts)
+  (let [{:keys [^RepositorySystem system
+                ^DefaultRepositorySystemSession  session
+                ^Artifact artifact
+                remote-repos]} (u.mvn/repository-system name "[0,)" opts)
         req (doto (VersionRangeRequest.)
               (.setArtifact artifact)
               (.setRepositories remote-repos))]
