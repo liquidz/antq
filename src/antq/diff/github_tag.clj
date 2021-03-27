d5b4ee709d2ad9090beeb3105c415600d9946e7a
liquidz <liquidz.uo@gmail.com>
2021-03-28 07:35:49 +0900

    Fix diff url for github-tag to choose similar one if exact one does not exist

--- src/antq/diff/github_tag.clj
+++ src/antq/diff/github_tag.clj
@@ -1,11 +1,21 @@
 (ns antq.diff.github-tag
   (:require
    [antq.diff :as diff]
+   [antq.util.git :as u.git]
    [clojure.string :as str]))
+
+(defn- exact-or-included
+  [coll ^String target]
+  (or (some #(and (= target %) %) coll)
+      (first (filter #(str/includes? % target) coll))))
 
 (defmethod diff/get-diff-url :github-tag
   [dep]
-  (format "https://github.com/%s/compare/%s...%s"
-          (str/join "/" (take 2 (str/split (:name dep) #"/")))
-          (:version dep)
-          (:latest-version dep)))
+  (let [url (format "https://github.com/%s"
+                    (str/join "/" (take 2 (str/split (:name dep) #"/"))))
+        tags (u.git/tags-by-ls-remote url)
+        current (or (exact-or-included tags (:version dep))
+                    (:version dep))
+        latest (or (exact-or-included tags (:latest-version dep))
+                   (:latest-version dep))]
+    (format "%s/compare/%s...%s" url current latest)))
