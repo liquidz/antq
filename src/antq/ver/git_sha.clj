2fd1e5a6e788f24af779340b4153bca4e16a0f33
liquidz <liquidz.uo@gmail.com>
2021-09-19 05:58:48 +0900

    Update to use antq.util.git

--- src/antq/ver/git_sha.clj
+++ src/antq/ver/git_sha.clj
@@ -1,29 +1,16 @@
 (ns antq.ver.git-sha
   (:require
+   [antq.util.git :as u.git]
    [antq.ver :as ver]
    [clojure.java.shell :as sh]
    [clojure.string :as str]))
 
-(defn- extract-head-sha
-  [ls-remote-resp]
-  (some->> (:out ls-remote-resp)
-           (str/split-lines)
-           (some (fn [line]
-                   (let [[sha ref-name] (str/split line #"\t" 2)]
-                     (and (= "HEAD" ref-name)
-                          sha))))))
-
-(defn- git-ls-remote
-  [url]
-  (sh/sh "git" "ls-remote" url))
-
 (defmethod ver/get-sorted-versions :git-sha
   [dep]
-  (let [resp (->> (get-in dep [:extra :url])
-                  (git-ls-remote))]
-    (or (some-> (extract-head-sha resp)
-                (vector))
-        [])))
+  (or (some-> (get-in dep [:extra :url])
+              (u.git/head-sha-by-ls-remote)
+              (vector))
+      []))
 
 (defmethod ver/latest? :git-sha
   [dep]
