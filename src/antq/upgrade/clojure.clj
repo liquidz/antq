8d0dbac8d14a71edd853be21861a7af9cb936038
liquidz <liquidz.uo@gmail.com>
2021-09-19 06:41:54 +0900

    Update to support upgrading :tag and :git/tag

--- src/antq/upgrade/clojure.clj
+++ src/antq/upgrade/clojure.clj
@@ -2,6 +2,7 @@
   (:require
    [antq.upgrade :as upgrade]
    [antq.util.dep :as u.dep]
+   [antq.util.git :as u.git]
    [antq.util.zip :as u.zip]
    [rewrite-clj.zip :as z]))
 
@@ -17,20 +18,62 @@
         (z/find-value loc z/right :git/sha))
     (z/find-value loc z/right :mvn/version)))
 
+(defmulti replace-versions
+  (fn [_loc version-checked-dep]
+    (:type version-checked-dep)))
+
+(defmethod replace-versions :default
+  [loc version-checked-dep]
+  (some-> loc
+          (z/find-value z/right :mvn/version)
+          (z/right)
+          (z/replace (:latest-version version-checked-dep))))
+
+(defmethod replace-versions :git-sha
+  [loc version-checked-dep]
+  (some-> (or (z/find-value loc z/right :sha)
+              (z/find-value loc z/right :git/sha))
+          (z/right)
+          (z/replace (:latest-version version-checked-dep))))
+
+(defmethod replace-versions :git-tag-and-sha
+  [loc version-checked-dep]
+  (let [tag-loc (some-> (or (z/find-value loc z/right :tag)
+                            (z/find-value loc z/right :git/tag))
+                        (z/right)
+                        (z/replace (:latest-version version-checked-dep))
+                        (z/up)
+                        (z/down))
+        sha-loc (when tag-loc
+                  (or (z/find-value tag-loc z/right :sha)
+                      (z/find-value tag-loc z/right :git/sha)))]
+    (cond
+      (and tag-loc sha-loc)
+      (let [{:keys [latest-version extra]} version-checked-dep
+            ;; NOTE: the result of `git ls-remote` should be cached by `memoize`
+            new-sha (-> (u.git/tag-sha-by-ls-remote (:url extra) latest-version)
+                        (subs 0 (count (:sha extra))))]
+        (-> sha-loc
+            (z/right)
+            (z/replace new-sha)))
+
+      tag-loc
+      tag-loc
+
+      :else
+      loc)))
+
 (defn upgrade-dep
   [loc version-checked-dep]
   (let [name-set (u.dep/name-candidates (:name version-checked-dep))]
     (loop [loc loc]
       (if-let [loc (z/find-value loc z/next name-set)]
         (recur (if (in-deps? loc)
-                 (some-> loc
-                         ;; move to map
-                         (z/right) (z/down)
-                         ;; find version key
-                         (find-version-key version-checked-dep)
-                         ;; replace
-                         (z/right)
-                         (z/replace (:latest-version version-checked-dep)))
+                 (or (some-> loc
+                             ;; move to map
+                             (z/right) (z/down)
+                             (replace-versions version-checked-dep))
+                     (z/next loc))
                  (z/next loc)))
         (u.zip/move-to-root loc)))))
 
