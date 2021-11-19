7c6cf25e72b46c799bdef5b433ff20b1b0dc6d41
liquidz <liquidz.uo@gmail.com>
2021-11-20 08:39:50 +0900

    feat: Add antq.changelog
     #84

---
+++ src/antq/changelog.clj
@@ -0,0 +1,85 @@
+(ns antq.changelog
+  (:require
+   [antq.log :as log]
+   [antq.util.dep :as u.dep]
+   [antq.util.maven :as u.mvn]
+   [antq.util.url :as u.url]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.tools.gitlibs :as gitlibs]))
+
+(def changelog-filenames
+  #{"changelog.adoc"
+    "changelog.md"
+    "changelog.org"
+    "changes.md"})
+
+(defn- silent-procure
+  [url lib rev]
+  (binding [*err* (java.io.StringWriter.)]
+    (gitlibs/procure url lib rev)))
+
+(defn- get-root-file-names
+  [url lib rev]
+  (when-let [dir (silent-procure url lib rev)]
+    (seq (.list (io/file dir)))))
+
+(defmulti get-git-url
+  (fn [version-checked-dep]
+    (:type version-checked-dep)))
+
+(defmethod get-git-url :default
+  [_dep]
+  nil)
+
+(defmethod get-git-url :git-sha
+  [dep]
+  (get-in dep [:extra :url]))
+
+(defmethod get-git-url :github-tag
+  [dep]
+  (format "https://github.com/%s"
+          (str/join "/" (take 2 (str/split (:name dep) #"/")))))
+
+(defmethod get-git-url :java
+  [dep]
+  (u.dep/get-scm-url dep))
+
+(defn get-changelog-url
+  [{:as version-checked-dep :keys [latest-version]}]
+  (when-let [url (get-git-url version-checked-dep)]
+    (cond
+      (str/starts-with? url "https://github.com/")
+      (let [file-names (get-root-file-names url
+                                            (symbol (:name version-checked-dep))
+                                            latest-version)
+            changelog (when file-names
+                        (some #(and (contains? changelog-filenames (str/lower-case %)) %)
+                              file-names))]
+        (when changelog
+          (str (u.url/ensure-git-https-url url)
+               "blob/" latest-version "/" changelog)))
+
+      :else
+      (do (log/warning (str "Changelog is not supported for " url))
+          nil))))
+
+
+
+
+(comment
+  (require 'antq.record)
+  ;(require 'antq.util.function)
+  (get-changelog-url
+   (antq.record/map->Dependency {:type :git-sha
+                                 :name "com.github.liquidz/antq"
+                                 :extra {:url "https://github.com/liquidz/antq.git"}
+                                 :version "1"
+                                 :latest-version "9b664f4b05be0d03366d418b1c4b50c5329726b4"}))
+
+  (get-changelog-url
+   (antq.record/map->Dependency {:type :java
+                                 :name "com.github.liquidz/antq"
+                                 :version "1.1.0"
+                                 :latest-version "1.2.0"
+                                 :repositories u.mvn/default-repos})))
---
+++ test/antq/changelog_test.clj
@@ -0,0 +1,44 @@
+(ns antq.changelog-test
+  (:require
+   [antq.changelog :as sut]
+   [antq.record :as r]
+   [antq.util.dep :as u.dep]
+   [clojure.test :as t]))
+
+(t/deftest get-changelog-url-test
+  (t/testing "java"
+    (let [dep (r/map->Dependency {:type :java
+                                  :name "foo/java"
+                                  :version "1.0"
+                                  :latest-version "2.0"})]
+      (with-redefs [u.dep/get-scm-url (constantly "https://github.com/foo/java")
+                    sut/get-root-file-names (constantly ["CHANGELOG.md"])]
+        (t/is (= "https://github.com/foo/java/blob/2.0/CHANGELOG.md"
+                 (sut/get-changelog-url dep))))))
+
+  (t/testing "git-sha"
+    (let [dep (r/map->Dependency {:type :git-sha
+                                  :name "foo/git-sha"
+                                  :extra {:url "https://github.com/foo/git-sha"}
+                                  :version "1.0"
+                                  :latest-version "2.0"})]
+      (with-redefs [sut/get-root-file-names (constantly ["CHANGELOG.adoc"])]
+        (t/is (= "https://github.com/foo/git-sha/blob/2.0/CHANGELOG.adoc"
+                 (sut/get-changelog-url dep))))))
+
+  (t/testing "github-tag"
+    (let [dep (r/map->Dependency {:type :github-tag
+                                  :name "foo/github-tag"
+                                  :version "1.0"
+                                  :latest-version "2.0"})]
+      (with-redefs [sut/get-root-file-names (constantly ["CHANGELOG.org"])]
+        (t/is (= "https://github.com/foo/github-tag/blob/2.0/CHANGELOG.org"
+                 (sut/get-changelog-url dep)))))))
+
+(t/deftest get-changelog-url-not-supported-test
+  (let [dep (r/map->Dependency {:type :git-sha
+                                :name "foo/git-sha"
+                                :extra {:url "https://gitlab.com/not/supported"}
+                                :version "1.0"
+                                :latest-version "2.0"})]
+    (t/is (nil? (sut/get-changelog-url dep)))))
