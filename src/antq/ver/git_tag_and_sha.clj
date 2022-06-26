152f832e228c90de1a3a4d4152d5d0a2da4432f6
liquidz <liquidz.uo@gmail.com>
2022-06-26 11:53:04 +0900

    feat: Update to show timed out message for git tag and sha deps

--- src/antq/ver/git_tag_and_sha.clj
+++ src/antq/ver/git_tag_and_sha.clj
@@ -1,5 +1,6 @@
 (ns antq.ver.git-tag-and-sha
   (:require
+   [antq.util.exception :as u.ex]
    [antq.util.git :as u.git]
    [antq.util.ver :as u.ver]
    [antq.ver :as ver]
@@ -7,13 +8,19 @@
 
 (defmethod ver/get-sorted-versions :git-tag-and-sha
   [dep _options]
-  (or (some->> (get-in dep [:extra :url])
-               (u.git/tags-by-ls-remote)
-               (filter (comp u.ver/sem-ver?
-                             u.ver/remove-qualifiers
-                             u.ver/normalize-version))
-               (sort (fn [& args]
-                       (apply version/version-compare
-                              (map u.ver/normalize-version args))))
-               (reverse))
-      []))
+  (try
+    (or (some->> (get-in dep [:extra :url])
+                 (u.git/tags-by-ls-remote)
+                 (filter (comp u.ver/sem-ver?
+                               u.ver/remove-qualifiers
+                               u.ver/normalize-version))
+                 (sort (fn [& args]
+                         (apply version/version-compare
+                                (map u.ver/normalize-version args))))
+                 (reverse))
+        [])
+    (catch Exception ex
+      (if (u.ex/ex-timeout? ex)
+        [ex]
+        (throw ex)))))
+
