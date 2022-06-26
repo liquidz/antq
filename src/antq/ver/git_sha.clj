c192ae270281661819c3cba2ef088163a5e87ec2
liquidz <liquidz.uo@gmail.com>
2022-06-26 11:50:20 +0900

    feat: Update to show timed out message for git sha deps

--- src/antq/ver/git_sha.clj
+++ src/antq/ver/git_sha.clj
@@ -1,18 +1,27 @@
 (ns antq.ver.git-sha
   (:require
+   [antq.util.exception :as u.ex]
    [antq.util.git :as u.git]
    [antq.ver :as ver]))
 
 (defmethod ver/get-sorted-versions :git-sha
   [dep _options]
-  (or (some-> (get-in dep [:extra :url])
-              (u.git/head-sha-by-ls-remote)
-              (vector))
-      []))
+  (try
+    (or (some-> (get-in dep [:extra :url])
+                (u.git/head-sha-by-ls-remote)
+                (vector))
+        [])
+    (catch Exception ex
+      (if (u.ex/ex-timeout? ex)
+        [ex]
+        (throw ex)))))
 
 (defmethod ver/latest? :git-sha
   [dep]
   (let [current (some-> dep :version)
-        latest (some-> dep :latest-version
-                       (subs 0 (count current)))]
-    (= latest current)))
+        latest (some-> dep :latest-version)]
+    (if (and (string? current)
+             (string? latest))
+      (= (subs latest 0 (count current))
+         current)
+      false)))
