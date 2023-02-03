3f648c819b162b556bc86caf3e16f0d87fce9eb8
liquidz <liquidz.uo@gmail.com>
2023-02-04 08:39:20 +0900

    fix: Fix leiningen upgrader to skip excluded deps
    cf. #199

--- test/antq/upgrade/leiningen_test.clj
+++ test/antq/upgrade/leiningen_test.clj
@@ -5,6 +5,7 @@
    [antq.test-helper :as h]
    [antq.upgrade :as upgrade]
    [antq.upgrade.leiningen]
+   [clojure.edn :as edn]
    [clojure.java.io :as io]
    [clojure.test :as t]))
 
@@ -18,12 +19,23 @@
                        :file
                        (slurp)
                        (dep.lein/extract-deps ""))
-        to-deps (->> java-dep
-                     (upgrade/upgrader)
-                     (dep.lein/extract-deps ""))]
+        upgraded (upgrade/upgrader java-dep)
+        to-deps (dep.lein/extract-deps "" upgraded)]
     (t/is (= #{{:name "foo/core" :version {:- "1.0.0" :+ "9.0.0"}}
                {:name "foo/core" :version {:- "1.1.0" :+ "9.0.0"}}}
-             (h/diff-deps from-deps to-deps)))))
+             (h/diff-deps from-deps to-deps)))
+
+    (let [upgraded-sexpr (edn/read-string upgraded)
+          profiles (->> upgraded-sexpr
+                        (drop-while #(not= :profiles %))
+                        (second))]
+      (t/testing "same name deps should be upgraded"
+        (t/is (= '[:dependencies [[foo/core "9.0.0"]]]
+                 (:same-name profiles))))
+
+      (t/testing "excluded deps should not be upgraded"
+        (t/is (= '[:dependencies [[foo/core "0.0.1"]]]
+                 (:same-name-but-excluded profiles)))))))
 
 (t/deftest upgrade-meta-dep-test
   (let [meta-dep (r/map->Dependency {:project :leiningen
