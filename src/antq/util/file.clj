f9561ee256f3b0508afb5f4e43bf70896026352b
liquidz <liquidz.uo@gmail.com>
2023-03-21 05:45:09 +0900

    fix: Update util.file/normalize-path to normalize redundant paths

--- src/antq/util/file.clj
+++ src/antq/util/file.clj
@@ -1,10 +1,23 @@
 (ns antq.util.file
   (:require
    [antq.util.env :as u.env]
+   [clojure.java.io :as io]
    [clojure.string :as str]))
 
-(defn normalize-path
+(defn- normalize-home
   [file-path]
   (if-let [home (u.env/getenv "HOME")]
     (str/replace file-path home "~")
     file-path))
+
+(defn- normalize-path*
+  [file-path]
+  (-> (io/file file-path)
+      (.toPath)
+      (.normalize)
+      (str)))
+
+(def ^{:malli/schema [:=> [:cat 'string?] 'string?]}
+  normalize-path
+  (comp normalize-home
+        normalize-path*))
--- test/antq/util/file_test.clj
+++ test/antq/util/file_test.clj
@@ -5,6 +5,14 @@
    [clojure.test :as t]))
 
 (t/deftest normalize-path-test
-  (with-redefs [u.env/getenv {"HOME" "/home/foo"}]
-    (t/is (= "/path/to/bar" (sut/normalize-path "/path/to/bar")))
-    (t/is (= "~/bar" (sut/normalize-path "/home/foo/bar")))))
+  (t/testing "HOME"
+    (with-redefs [u.env/getenv {"HOME" "/home/foo"}]
+      (t/is (= "/path/to/bar" (sut/normalize-path "/path/to/bar")))
+      (t/is (= "~/bar" (sut/normalize-path "/home/foo/bar")))))
+
+  (t/testing "Redundant path"
+    (t/is (= "/path/to/bar" (sut/normalize-path "/path/to/./foo/../bar"))))
+
+  (t/testing "HOME and Redundant path"
+    (with-redefs [u.env/getenv {"HOME" "/home/foo"}]
+      (t/is (= "~/bar" (sut/normalize-path "/home/./bar/../foo/bar"))))))
