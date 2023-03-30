e13b9d08118a8030c994c33de49d60d721f4bffd
liquidz <liquidz.uo@gmail.com>
2023-03-31 06:01:57 +0900

    feat: Add no-output-reporter

--- src/antq/report.clj
+++ src/antq/report.clj
@@ -2,6 +2,8 @@
   (:require
    [antq.log :as log]))
 
+(def no-output-reporter "__NO_OUTPUT__")
+
 (defmulti reporter
   (fn [_deps options]
     (:reporter options)))
@@ -9,6 +11,10 @@
 (defmethod reporter :default
   [_ options]
   (log/error (str "Unknown reporter: " (:reporter options))))
+
+(defmethod reporter no-output-reporter
+  [_ _]
+  nil)
 
 (defmulti init-progress
   (fn [_deps options]
