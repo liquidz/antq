ae30df1afcc6859359e883b78ca51f0f702e05e9
liquidz <liquidz.uo@gmail.com>
2023-04-01 08:05:23 +0900

    feat: Add antq.util.file/detect-project

--- src/antq/util/file.clj
+++ src/antq/util/file.clj
@@ -1,5 +1,6 @@
 (ns antq.util.file
   (:require
+   [antq.constant.project-file :as const.project-file]
    [antq.util.env :as u.env]
    [clojure.java.io :as io]
    [clojure.string :as str]))
@@ -21,3 +22,15 @@
   normalize-path
   (comp normalize-home
         normalize-path*))
+
+(defn detect-project
+  [file-path]
+  (condp #(str/ends-with? %2 %1) file-path
+    const.project-file/babashka :clojure
+    const.project-file/boot :boot
+    const.project-file/clojure-cli :clojure
+    const.project-file/gradle :gradle
+    const.project-file/leiningen :leiningen
+    const.project-file/maven :pom
+    const.project-file/shadow-cljs :shadow-cljs
+    ::unknown))
--- test/antq/util/file_test.clj
+++ test/antq/util/file_test.clj
@@ -16,3 +16,13 @@
   (t/testing "HOME and Redundant path"
     (with-redefs [u.env/getenv {"HOME" "/home/foo"}]
       (t/is (= "~/bar" (sut/normalize-path "/home/./bar/../foo/bar"))))))
+
+(t/deftest detect-project-test
+  (t/are [expected file-path] (= expected (sut/detect-project file-path))
+    :clojure "/path/to/deps.edn"
+    :clojure "/path/to/bb.edn"
+    :boot "/path/to/build.boot"
+    :gradle "/path/to/build.gradle"
+    :leiningen "/path/to/project.clj"
+    :shadow-cljs "/path/to/shadow-cljs.edn"
+    ::sut/unknown "/path/to/invalid"))
