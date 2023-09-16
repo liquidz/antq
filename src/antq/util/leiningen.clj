819e11f30c7004e7bd865d0a0945a19bb4180557
Taiju Aoki <aokitaiju0513@gmail.com>
2023-09-16 16:08:33 +0900

    feature: resolve private maven repo with GPG by leiningen

--- src/antq/util/leiningen.clj
+++ src/antq/util/leiningen.clj
@@ -1,7 +1,13 @@
 (ns antq.util.leiningen
   (:require
    [antq.util.env :as u.env]
-   [clojure.string :as str]))
+   [clojure.string :as str]
+   [clojure.java.shell :as shell]
+   [clojure.java.io :as io]))
+
+(defn lein-home
+  []
+  (io/file (System/getProperty "user.home") ".lein"))
 
 (defn- env-name
   "cf. https://github.com/technomancy/leiningen/blob/master/doc/DEPLOY.md#credentials-in-the-environment"
@@ -16,6 +22,36 @@
 
     :else
     nil))
+
+(defn- gpg-program
+  []
+  (or (System/getenv "LEIN_GPG") "gpg"))
+
+(defn- gpg
+  [& args]
+  (try
+    (apply shell/sh (gpg-program) args)
+    (catch java.io.IOException e
+      {:exit 1 :err (.getMessage e)})))
+
+(defn- credentials-fn
+  "cf. https://leiningen.org/reference.html"
+  ([] (let [cred-file (io/file (lein-home) "credentials.clj.gpg")]
+        (if (.exists cred-file)
+          (credentials-fn cred-file))))
+  ([file]
+   (let [{:keys [out err exit]} (gpg "--quiet" "--batch"
+                                     "--decrypt" "--" (str file))]
+     (if (pos? exit)
+       (binding [*out* *err*]
+         (println "Could not decrypt credentials from" (str file))
+         (println err)
+         (println "See `lein help gpg` for how to install gpg."))
+       (read-string out)))))
+
+(defn read-credential
+  []
+  (credentials-fn))
 
 (defn env
   [kw]
